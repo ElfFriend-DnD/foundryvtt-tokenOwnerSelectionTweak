f0bf5f2ea64857771f16d8f32bc6f5f58c39ef07
akrigline <a.krigline@gmail.com>
2020-09-03 21:58:11 -0400

    initial commit

---
+++ src/module/preloadTemplates.ts
@@ -0,0 +1,7 @@
+export const preloadTemplates = async function() {
+	const templatePaths = [
+		// Add paths to "modules/foundryvtt-tokenOwnerSelectionTweak/templates"
+	];
+
+	return loadTemplates(templatePaths);
+}
