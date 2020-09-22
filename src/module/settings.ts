6c635513321c72f3839034ad4ca971eebfbf1512
akrigline <a.krigline@gmail.com>
2020-09-21 22:41:39 -0400

    tweak the onCreate override to work closer to what foundry Core does

--- src/module/settings.ts
+++ src/module/settings.ts
@@ -1,14 +1,12 @@
 import { MODULE_ID, MySettings } from '../constants';
-import { resetSight } from '../helpers';
 
 export const registerSettings = function () {
   game.settings.register(MODULE_ID, MySettings.preserve, {
     name: 'Preserve token owner selection',
     default: true,
     type: Boolean,
-    scope: 'world',
+    scope: 'client',
     config: true,
     hint: "Default Foundry behavior is to change the owners' selections to the created token when it is created.",
-    onChange: (value) => resetSight(),
   });
 };
